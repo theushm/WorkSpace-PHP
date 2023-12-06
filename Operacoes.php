70801b42aa64914e672809fa18bd0edeab507cf5
Matheus Henrique Menezes Gomes da Silva <theushmpe@gmail.com>
2023-12-05 21:49:26 -0300

    Modificação 05/12/23 21:49 - Casa

--- Operacoes.php
+++ Operacoes.php
@@ -6,9 +6,13 @@ $imc = $peso / $altura ** 2;
 
 echo "O seu imc é: $imc";
 
-echo "Você está acima do peso!" . PHP_EOL;
+echo "Você está acima do peso!".PHP_EOL;
 
-if ($imc > 24);
+if($imc > 24) {
+    echo "seu imc é tal";
+    echo "é 24";
+}
+
 
 //  Próximos passos: Adicionar as frases:
 // Seu imc está normal (entre 18,6 e 24,9)
