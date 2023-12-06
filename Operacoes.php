29d697ed62fa693f9c8279ba7a079ece0b325756
Matheus Henrique Menezes Gomes da Silva <theushmpe@gmail.com>
2023-12-05 21:20:13 -0300

    Modificação 05/12/2023 Casa

---
+++ Operacoes.php
@@ -0,0 +1,15 @@
+<?php
+
+$peso = 70;
+$altura = 1.70;
+$imc = $peso / $altura ** 2;
+
+echo "O seu imc é: $imc";
+
+echo "Você está acima do peso!" . PHP_EOL;
+
+if ($imc > 24);
+
+/* Próximos passos: Adicionar as frases:
+Seu imc está normal (entre 18,6 e 24,9)
+Você está com sobrepeso (entre 25 e 29,9) /*
---
+++ String.php
@@ -0,0 +1,4 @@
+<?php
+
+$idade = 23;
+echo 'Olá mundo minha idade é ' . $idade;
